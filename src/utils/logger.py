e21c15ed8693f990228697809165a2e0fb36e518
cardcash <terencecawleynew@gmail.com>
2026-06-07 13:47:22 +0800

    Create logger.py

---
+++ src/utils/logger.py
@@ -0,0 +1,13 @@
+import sys
+from loguru import logger
+
+# Configure the logger
+logger.remove()
+logger.add(
+    sys.stderr,
+    format="<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level: <8}</level> | <cyan>{name}</cyan>:<cyan>{function}</cyan> - <level>{message}</level>",
+    level="INFO"
+)
+
+# Export the logger instance
+__all__ = ["logger"]
